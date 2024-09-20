22313be51eefa9ae10ca09b4b2d0502a28225366
23DP3AGALI <166391692+23DP3AGALI@users.noreply.github.com>
2024-09-20 05:57:30 +0000

    Speed Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,56 +6,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
-
-        // Scanner scanner = new Scanner(System.in);
-      
-      
-        // String input = scanner.nextLine();
-
-        // System.out.println("Sveiks " + input + "!");
-
-        // Scanner scanner = new Scanner(System.in);
-
-        // System.out.println("Ievadi savu vārdu: ");
-
-        // String name = scanner.nextLine();
-
-        // System.out.println();
-        // String lastname = scanner.nextLine();
-
-        // System.out.println();
-        // String group = scanner.nextLine();
-
-        // System.out.println();
-
-
-        // String text = "kaudkads teksts";
-        // int WholeNumber = 123;
-        // double floatingPoint = 3.141592653;
-        // boolean tureOrFalse = true;
-
-        // System.out.println("Musu teksts: " + text);
-        // System.out.println("Cipari:" + WholeNumber);
-        // System.out.println("Pi skaitlis: " + floatingPoint);
-        // System.out.println("True or false: " + tureOrFalse);
-
-
+  
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ievadiet 1.skaitlis:");
-        int number1 = Integer.valueOf(scanner.nextLine());
-        System.out.println("Ievadiet 2.skaitli:");
-        int number2 = Integer.valueOf(scanner.nextLine());
-        int summa = number1 + number2;
-        int starp = number1 - number2;
-        int reiz = number1 * number2;
-        System.out.println(number1 + "+" + number2 + "=" + summa);
-        System.out.println(number1 + "-" + number2 + "=" + starp);
-        System.out.println(number1 + "*" + number2 + "=" + reiz);
-        double average = (number1 + number2)/2;
-        System.out.println("(" + number1 + "+"+number2 + ")/2=" + average);
-
-
+        System.out.println("Give speed: ");
+        int speed = scanner.nextInt();
+        if (speed > 120) {
+            System.out.println("Speeding ticket");
+        
+        
+        
+        }
 
     }
 
