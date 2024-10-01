aefe32adc50802cda34d5971d8079bfca94ac138
23DP3AGALI <166391692+23DP3AGALI@users.noreply.github.com>
2024-10-01 14:04:20 +0000

    Part2-3 Counting

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,44 +6,16 @@ public class App
 {
     public static void main( String[] args )
     {
-  
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Give speed: ");
-        // int speed = scanner.nextInt();
-        // if (speed > 120) {
-        //     System.out.println("Speeding ticket");
-        
-        
-        
-        // }
 
+        
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ievadi savu balle");
-        int ball = scanner.nextInt();
-        if (ball < 0) {
-            System.out.println("Nevar būt!");
-        
 
-        } else if (0 < ball && ball <= 49) {
-            System.out.println("Nav sekmīgs");
-
-        } else if (50 <= ball && ball <= 59) {
-            System.out.println("1");
-
-        } else if (60 <= ball && ball <= 69) {
-            System.out.println("2");
-
-        } else if (70 <= ball && ball <= 79) {
-            System.out.println("3");
-        } else if (80 <= ball && ball <= 89) {
-            System.out.println("4");
-        } else if (90 <= ball && ball <= 100) {
-            System.out.println("5");
-        } else if (100 <= ball) {
-            System.out.println("INSANE!");
+        System.out.println("Ievadi jebkuru pozitivu skaitlu: ");
+        int user = Integer.valueOf(scanner.nextLine());
+        for (int i = 0; i <= user; i++) {
+            System.out.println(i);
         }
-
 
 
 
