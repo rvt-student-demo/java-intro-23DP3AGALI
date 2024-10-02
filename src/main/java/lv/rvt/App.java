3347c8a7e21babbb1c67f04d686fb93cde3bb221
23DP3AGALI <166391692+23DP3AGALI@users.noreply.github.com>
2024-10-02 07:15:00 +0000

    Repeaing breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,33 +1,48 @@
 package lv.rvt;
-
 import java.util.Scanner;
-
 public class App 
 {
     public static void main( String[] args )
-    {
-
-        
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Ievadi jebkuru pozitivu skaitlu: ");
-        int user = Integer.valueOf(scanner.nextLine());
-        
-        int sum = 0;
-
-        for (int i = 1; i <= user; i++) {
-            sum += i;
-        }
-
-        System.out.println(sum);
-        
-        
-
-
-
-    }
-
     
 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Last number? ");
+
+        
+        
+        int i=0;
+        int sum=0;
+        int odd=0;
+        int even=0;
+        System.out.println("Give numbers:");
+
+    while (true) {
+        
+        int sk = Integer.valueOf(scanner.nextLine());
+        
+        
+        if (sk==-1) {
+            System.out.println("Thx! Bye!");
+            break;
+        }
+        if (sk%2!=0) {
+            odd++;
+        }
+        else {
+            even++;
+        }
+        
+        i++;
+        sum=sum+sk;
+        continue;
+    }
+    double vid = (double)sum / i;
+    System.out.println("Sum: "+ sum);
+    System.out.println("Numbers: " + i);
+    System.out.println("Average:" + vid);
+    System.out.println("Odd: " + odd);
+    System.out.println("Even: " + even);
+
+    }
 }
